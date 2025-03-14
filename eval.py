cac022616b78777e2756d8f06954d94acd4baea0
gcobs104628 <gcobs104628@gmail.com>
2025-03-14 22:25:21 +0800

    DRL assignment 2

---
+++ eval.py
@@ -0,0 +1,26 @@
+# custom_taxi_env.py
+import gym
+import numpy as np
+import time
+import random
+
+
+from xml.etree import ElementTree as ET
+import importlib.util
+import requests
+import argparse
+import torch
+import random
+import sys
+import importlib
+import env
+
+
+
+if __name__ == "__main__":
+    env.eval_score()  
+    
+
+
+
+    
